f8533afa573b93c939cde15050f3e2805a4426f6
Buczul <buczo4002@gmail.com>
2025-01-18 13:19:48 +0100

    Add files via upload

---
+++ lab1.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+
+int main()
+{
+	////1. Napisaæ program, który wypisuje na konsolê nr albumu studenta(osoby rozwi¹zuj¹cej program).
+	
+	//printf("134894\n");
+
+	////2. Napisaæ program, który sprawdza, czy podana liczba ca³kowita, wczytana z klawiatury jest parzysta, czy nieparzysta. (operator %).
+
+	//printf("Podaj liczbe w celu sprawdzenia czy jest parzysta:\n");
+	//int a1;
+	//scanf("%d", &a1);
+	//if (a1 % 2 == 0) printf("Liczba %d jest parzysta.\n", a1);
+	//else printf("Liczba %d jest nieparzysta.\n", a1);
+
+	////3. Napisaæ program, który pozwala u¿ytkownikowi na wprowadzenie dwóch liczb zmiennoprzecinkowych,
+	////a nastêpnie zwraca informacjê, która z wprowadzonych liczb jest liczb¹ wiêksz¹.
+
+	//float a2, b2;
+	//printf("Podaj 2 liczby\nLiczba pierwsza: ");
+	//scanf("%f", &a2);
+	//printf("Liczba druga: ");
+	//scanf("%f", &b2);
+	//if (a2 > b2) printf("Liczba %f jest wieksza.", a2);
+	//else if (b2 > a2) printf("Liczba %f jest wieksza.", b2);
+	//else printf("Liczby sa rowne.");
+
+	////4. Napisaæ program, który pozwala u¿ytkownikowi na wprowadzenie czterech liczb
+	////zmiennoprzecinkowych, a nastêpnie zwraca œredni¹ arytmetyczn¹ z wprowadzonych liczb.
+
+	//float a3, b3, c3, d3;
+	//printf("Podaj 4 liczby w celu obliczenia z nich sredniej\nLiczba pierwsza: ");
+	//scanf("%f", &a3);
+	//printf("Liczba druga: ");
+	//scanf("%f", &b3);
+	//printf("Liczba trzecia: ");
+	//scanf("%f", &c3);
+	//printf("Liczba czwarta: ");
+	//scanf("%f", &d3);
+
+	//printf("Srednia z podanych liczb: %f", (a3 + b3 + c3 + d3) / 4);
+
+	////Napisaæ prosty kalkulator wykonuj¹cy dzia³anie matematyczne (+, -, /, *) na dwóch liczbach podanych z klawiatury, wykorzystuj¹c instrukcjê switch.
+
+	//float a4, b4;
+	//int c4;
+
+	//printf("KALKULATOR\nPodaj pierwsza liczbe: ");
+	//scanf("%f", &a4);
+	//printf(" 1 -> dodawanie\n 2 -> odejmowanie\n 3 -> dzielenie\n 4 -> mnozenie\nPodaj symbol dzialania jakie chcesz wykonac:");
+	//scanf("%d", &c4);
+	//printf("Podaj druga liczbe: ");
+	//scanf("%f", &b4);
+
+	//switch (c4)
+	//{
+	//case 1:printf("%f", a4 + b4);
+	//	break;
+	//case 2:printf("%f", a4 - b4);
+	//	break;
+	//case 3: if (b4 != 0) printf("%f", a4 / b4);
+	//	  else printf("Nie dzielimy przez 0");
+	//	break;
+	//case 4:printf("%f", a4 * b4);
+	//	break;
+	//default:printf("Podano niedozwolone dzialanie");
+	//	break;
+	//}
+
+
+	return 0;
+}
